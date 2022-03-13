b72a35db3d940948fbb85b72fd8cd76c7292c9b0
Jan <juan.perezlosa@gmail.com>
2022-03-13 12:19:00 +0100

    l18mSelectPipe y tareas

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -8,12 +8,31 @@ import { Component } from '@angular/core';
 })
 export class NoComunesComponent {
 
-  nombre: string = 'Susana';
-  genero: string = 'femenino';
+  //i18nSelect
+  nombre: string = 'Carlos';
+  genero: string = 'masculino';
 
   invitacionMapa = {
     'masculino': 'invitarlo',
     'femenino' : 'invitarla'
+  }
+
+  //i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Juan', 'Eduardo', 'Fernando'];
+
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos 2 clientes esperando',
+    'other': 'tenemos # clientes esperando.'
+  }
+
+  cambiarCliente(){
+    this.nombre = 'Marta';
+    this.genero = 'femenino';
+  }
+  borrarCliente(){
+    this.clientes.pop();
   }
  
 }
